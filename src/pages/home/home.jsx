49d509dca3ae74e5fb275139e328d19fac59dbd4
ayere progress <ayeremichael20@gmail.com>
2026-02-18 19:02:01 +0100

    Fixed the console error

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -718,7 +718,7 @@ function Home() {
       
       <Footer />
 
-      <style jsx>{`
+      <style>{`
         @keyframes blob {
           0%, 100% { transform: translate(0, 0) scale(1); }
           25% { transform: translate(20px, -50px) scale(1.1); }
