865c98cf43e60f7b491138d33125597f583c5361
Web3ProdigyDev <gj09042003@gmail.com>
2026-02-18 19:05:21 +0100

    Fix attempt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+/* eslint-disable import/first */
+import { Buffer } from 'buffer';
+window.Buffer = Buffer;
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
