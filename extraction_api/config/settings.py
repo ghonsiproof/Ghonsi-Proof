160e29dcfe0ba07272d0eff955aa16f34c4b67fd
peter isaiah <49757859+isaiahpeter@users.noreply.github.com>
2026-02-23 18:11:33 +0100

    Configure TEMPLATES setting in settings.py
    Replaced empty TEMPLATES list with a configured Django template settings.

--- extraction_api/config/settings.py
+++ extraction_api/config/settings.py
@@ -64,22 +64,20 @@ MIDDLEWARE = [
 ]
 ROOT_URLCONF = 'config.urls'
 
-TEMPLATES = []
-
-#TEMPLATES = [
-#    {
-#        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-#        'DIRS': [],
-#        'APP_DIRS': True,
-#        'OPTIONS': {
-#            'context_processors': [
-#                'django.template.context_processors.request',
-#                'django.contrib.auth.context_processors.auth',
-#                'django.contrib.messages.context_processors.messages',
-#            ],
-#        },
-#    },
-#]
+TEMPLATES = [
+    {
+         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+         'DIRS': [],
+         'APP_DIRS': True,
+         'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
